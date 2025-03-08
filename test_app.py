bbb077a51b7292fa579e123049f37bcdab1f51bd
Guillermo Rodrigues <grodrigues217@gmail.com>
2025-03-08 17:18:21 +0100

    test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
-import pytest
 from app import dias_vividos
 
+
 def test_dias_vividos():
-    # Para 10 años se espera 3650 días (10 * 365)
-    assert dias_vividos(10) == 3650, "Error: 10 años deberían dar 3650 días."
+    # Para 10 años se esperan 3650 días
+    assert dias_vividos(10) == 3650
