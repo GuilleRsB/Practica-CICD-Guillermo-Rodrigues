b87c411258aa68ce5861ed148a5771326f435b19
Guillermo Rodrigues <grodrigues217@gmail.com>
2025-03-22 12:55:57 +0100

    test_app

--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
 import unittest
 from app.app import app, dias_vividos
 
+
 class TestDiasVividos(unittest.TestCase):
+
     def test_dias_vividos(self):
         self.assertEqual(dias_vividos(10), 3650)
 
@@ -12,14 +14,27 @@ class TestDiasVividos(unittest.TestCase):
     def test_index_get(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Calcula los d\xc3\xadas vividos', response.data)
+        self.assertIn(
+            b'Calcula los d\xc3\xadas vividos',
+            response.data
+        )
 
     def test_index_post_valid(self):
-        response = self.client.post('/', data={'nombre': 'Guillermo', 'edad': '25'})
+        response = self.client.post(
+            '/', data={'nombre': 'Guillermo', 'edad': '25'}
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Guillermo, has vivido aproximadamente', response.data)
+        self.assertIn(
+            b'Guillermo, has vivido aproximadamente',
+            response.data
+        )
 
     def test_index_post_invalid(self):
-        response = self.client.post('/', data={'nombre': 'Guillermo', 'edad': 'abc'})
+        response = self.client.post(
+            '/', data={'nombre': 'Guillermo', 'edad': 'abc'}
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Error: La edad debe ser un n\xc3\xbamero entero.', response.data)
+        self.assertIn(
+            b'Error: La edad debe ser un n\xc3\xbamero entero.',
+            response.data
+        )
